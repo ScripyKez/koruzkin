ef980412a29474af16ca0c08c657404d9f8eca8d
SkripyKez <koruzkin@icloud.com>
2025-04-22 22:53:48 +0300

    init

---
+++ front/src/services/auth.service.ts
@@ -0,0 +1,40 @@
+import axios from "axios"
+
+const API_URL = "http://localhost:8080/api/auth/"
+
+export const register = (username: string, email: string, password: string) => {
+  return axios.post(API_URL + "signup", {
+    username,
+    email,
+    password,
+  })
+}
+
+export const login = (username: string, password: string) => {
+  console.log(username, password)
+  return axios
+    .post(API_URL + "signin", {
+      username,
+      password,
+    })
+    .then(response => {
+      console.log(response, response.headers)
+
+      if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data))
+      }
+
+      return response.data
+    })
+}
+
+export const logout = () => {
+  localStorage.removeItem("user")
+}
+
+export const getCurrentUser = () => {
+  const userStr = localStorage.getItem("user")
+  if (userStr) return JSON.parse(userStr)
+
+  return null
+}
---
+++ server/app/config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  USERNAME: "admin",
+  PASSWORD: "AK*1h2zS",
+  HOST: "rapaquoclo.beget.app",
+  PORT: 27017,
+  DB: "admin",
+}
