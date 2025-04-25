346fbc896a935d6b6ce33eb6aeb7c5cfa1fb1eed
SkripyKez <koruzkin@icloud.com>
2025-04-25 16:15:36 +0300

    fix

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,11 +1,13 @@
-const {
-  DB_USER,
-  DB_PASSWORD,
-  DB_HOST,
-  DB_PORT,
-  DB_NAME,
-} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = {
+  DB_HOST: "localhost",
+  DB_USER: "root",
+  DB_PASSWORD: "123456",
+  DB_NAME: "bezkoder_db",
+  DB_PORT: "27017",
+
+  NODE_DOCKER_PORT: "8080",
+}
 
 module.exports = {
-  url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
-};
+  url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
+}
