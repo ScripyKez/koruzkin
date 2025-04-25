45f8223858daca58b5a9b990b5522c53fb766ecc
SkripyKez <koruzkin@icloud.com>
2025-04-25 14:14:07 +0300

    123

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
 
 app.use(
   cookieSession({
